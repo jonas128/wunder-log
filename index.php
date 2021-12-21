786e035a095781736a0806231b7321bc3f780534
jonas128 <jonasm@outlook.com>
2021-12-22 00:13:28 +0100

    Add config.php and database.db

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/views/header.php'; ?>
+
+<article>
+    <h1><?php echo $config['title']; ?></h1>
+    <p></p>
+
+
+</article>
+
+<?php require __DIR__ . '/views/footer.php'; ?>
